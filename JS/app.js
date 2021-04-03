0b336879421c09344533c2d0b6bd8fad01bfd4aa
Adel Kuanysheva <akuanysheva@my.bcit.ca>
2021-04-02 23:53:05 -0400

    Function 2 added, save and cancel button functions created

--- JS/app.js
+++ JS/app.js
@@ -16,6 +16,40 @@ btn.addEventListener('click', function() {
     }
 })
 
+function openNote() {
+    document.getElementById("noteArea").innerHTML =
+        "<div class = 'note'>"
+            +  "<label for='story'><button type = 'button'>+</button></label>"
+            + "<textarea id='story' name='story' rows='10' cols='33'>your note here</textarea>"
+        + "<div class='savedelete'>"
+        + "<button type = 'button' id='b1' onclick = 'saveNote()'>save</button>"
+        + "<button type = 'button' id='b2' onclick = 'cancelNote()'>cancel</button></div></div>"
+} 
+
+
+var x = 1;
+var noteArray = Array();
+
+function saveNote () {
+    noteArray[x] = {Title: "Note " + x,  Body: document.getElementById("story").value};
+    x++;
+    document.getElementById("noteArea").innerHTML =
+    "<button id = 'addNewNote' type= 'button' onclick='openNote()'>+ create a new note</button>"
+}
+  
+function cancelNote() {
+    document.getElementById("noteArea").innerHTML =
+    "<button id = 'addNewNote' type= 'button' onclick='openNote()'>+ create a new note</button>"
+}
+
+function openMenu() {
+    document.getElementById("sideMenu").innerHTML = 
+    " <div class='note_nav'>"
+    + "<button class='menu_button' type = 'button'>&lt&lt</button>"
+    + "<ul>"
+        + "<li class='mynotes'><a href=''>my notes</a></li>  </ul></div>"
+
+}
 
 /* Plans to create functions in Note-ly app. */ 
 
@@ -35,18 +69,20 @@ btn.addEventListener('click', function() {
 
 /*  Create a function that adds the html from note.html into index.html:
         - function will not take any parameters
-        - It will use .createElement features to add literals
-        - Use template literals to add element of class "note", inside will be:
+        - It will use .getElementbyId feature to add html literals
+        - Add element of class "note", inside will be:
             - label tag, button
             - textarea tag
             - div tag, class "savedelete" - 2 buttons 
     Save button:
-        - use onclick to save the object inputed into a noteArray
-        - object will have a body and title 
-        - push() method 
-        - will undo the function above (remove the "note" class)
+        - use onclick to save the object inputed into a noteArray, **google this
+            - possibly use indexing, noteArray[x] = ... 
+        - object will have a body and title
+            - {Title: Note + x, Body: .. }
+        - replace noteArea with the addNewNote button (original state)
     Delete button:
         - will simply remove the "note" class
+        - replace the noteArea with the addNewNote button 
 */      
 
 
